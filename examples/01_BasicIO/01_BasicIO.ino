257dd39df0b6a92604ef8b1d7b1fc7183bfe8d20
Iskak Fatoni <iskakfatoni@gmail.com>
2026-02-15 18:23:06 +0700

    Create 01_BasicIO.ino

---
+++ examples/01_BasicIO/01_BasicIO.ino
@@ -0,0 +1,26 @@
+#include <IskakINO_ArduFast.h>
+
+// Definisi LED menggunakan FastPin untuk performa maksimal
+FastPin<LED_BUILTIN> MyLed;
+
+void setup() {
+    // Memulai sistem log dan serial
+    ArduFast.begin(115200);
+    
+    // Set mode pin melalui template FastPin
+    MyLed.mode(OUTPUT);
+}
+
+void loop() {
+    // Task 1: Kedipkan LED setiap 1 detik (ID: 0)
+    if (ArduFast.every(1000, 0)) {
+        MyLed.toggle();
+        ArduFast.log(F("LED Berkedip"));
+    }
+
+    // Task 2: Baca sensor setiap 5 detik (ID: 1)
+    if (ArduFast.every(5000, 1)) {
+        int val = ArduFast.readNorm(A0);
+        ArduFast.log(F("Nilai Sensor (0-1023)"), val);
+    }
+}
